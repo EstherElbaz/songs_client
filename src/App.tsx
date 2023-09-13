e0563191a8c79776e82b9c0bed250762261aed11
Esther Elbaz <133249336+EstherElbaz@users.noreply.github.com>
2023-09-13 18:43:39 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Songs from './components/songs';
+
+function App() {
+  
+  return (
+    <>
+    <Songs></Songs>
+    </>
+  );
+}
+
+export default App;
---
+++ src/models.tsx
@@ -0,0 +1,5 @@
+export class Song {
+    year: string = '2023';
+    songName: string = "";
+    band: string = "";
+}
